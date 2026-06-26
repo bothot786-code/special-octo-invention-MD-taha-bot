1f7142b7f0cbb9689733799a857cb642bf700928
bothot786-code <bothot786@gmail.com>
2026-06-26 17:08:20 +0500

    Create config.js

---
+++ config.js
@@ -0,0 +1,7 @@
+module.exports = {
+    botName: "TAHA MD BOT", // Bot ka naam
+    ownerName: "Taha Khan",
+    ownerNumber: "923XXXXXXXXX", // Apna WhatsApp number lagaein (Country code ke sath, bina + ke)
+    prefix: ".", // Bot commands ka prefix
+    stateName: "session" // Render disk ya session folder ka naam
+};
